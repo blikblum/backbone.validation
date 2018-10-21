b8288331d09f8be6e2d7842f271d3542d0d2c79e
Luiz Américo <luizamericop@gmail.com>
2018-10-21 13:22:31 -0300

    Respect attributes property passed through validate / set methods

--- test/attributesOption.js
+++ test/attributesOption.js
@@ -181,6 +181,33 @@ module.exports = {
                     refute.defined(errors);
                 }
             }
-        }
+        },
+        "through Model.validate options": {
+          beforeEach: function () {
+              Object.assign(this.model, Backbone.Validation.mixin)
+          },
+
+          "only the attributes in array should be validated": function () {
+              var errors = this.model.validate(undefined, {attributes: ['name', 'age']});
+              assert.defined(errors.name);
+              assert.defined(errors.age);
+              refute.defined(errors.password);
+              refute.defined(errors.email);
+          },
+
+          "when all the attributes in array are valid": {
+              beforeEach: function () {
+                  this.model.set({
+                      age: 1,
+                      name: 'hello',
+                      email: 'invalidemail'
+                  });
+              },
+              "validation will pass": function () {
+                  var errors = this.model.validate(undefined, {attributes: ['name', 'age']});
+                  refute.defined(errors);
+              }
+          }
+      }
     }
 }
